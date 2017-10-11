52a003fcd41dc1f4430d2e6f18b3d3d5209b7051
Nathan Campbell <nathancampbell@outlook.com>
2017-10-11 04:28:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.order(created_at: :desc).page(params[:page])
+    @songs = Song.order(title: :desc).page(params[:page])
   end
 
   def show
